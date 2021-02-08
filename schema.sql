3e20bbf0f5cd21d564d3eecaa748f7f142698b49
Luke Terrell <luke.terrell43@gmail.com>
2021-02-07 18:57:00 -0600

    created unique titles files to determine number of retiring employees for each title

--- schema.sql
+++ schema.sql
@@ -198,3 +198,17 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+-- Get titles of indivduals near retirement
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+-- INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY (e.emp_no);
